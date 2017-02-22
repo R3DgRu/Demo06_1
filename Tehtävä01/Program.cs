cbbf779eb11883ed2502a3e3a77c508c92a11f93
Batman <Batman@DESKTOP-BOJAOPV>
2017-02-22 18:29:32 +0200

    Tehtävä 1 valmis

--- Tehtävä01/Program.cs
+++ Tehtävä01/Program.cs
@@ -10,6 +10,23 @@ namespace Tehtävä01
     {
         static void Main(string[] args)
         {
-        }
+            string teksti = "oletus";
+
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter("file.txt"); // luo tekstitiedoston ohjelman kansioon
+
+            // kysyy käyttäjältä tekstiä kunnes tämä syöttää q
+            while (teksti != "q") {
+
+                Console.Write("Give a text line ('q' to exit):  ");
+                teksti = Console.ReadLine();
+                outputFile.WriteLine(teksti);
+            }
+            
+            outputFile.Close(); // sulkee tiedoston
+
+            // tulostaa tiedoston sisukset komentoriville
+            string text = System.IO.File.ReadAllText("file.txt");
+            System.Console.WriteLine("\nContents of file.txt:\n\n" + text);
+        } 
     }
 }
